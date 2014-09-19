dac6022d8037c40eb656a4942e06287bdfe86467
Hilmar <hilmar@hilmarp.com>
2014-09-19 11:58:51 +0000

    Tilbúið, local storage bætt við

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,15 @@
  */
 angular.module('mytodoApp')
 
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, localStorageService) {
 
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+
+    $scope.$watch('todos', function() {
+    	localStorageService.add('todos', $scope.todos.join('\n'));
+    }, true);
 
     $scope.addTodo = function() {
     	$scope.todos.push($scope.todo);
